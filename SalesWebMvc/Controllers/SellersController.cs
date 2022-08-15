5a71ab822226817c78b58abfca03e8db8506d831
Siraque <msiraque@hotmail.com>
2022-08-15 15:38:32 -0300

    SellerFormViewModel and Department select component

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SalesWebMvc.Models.ViewModels;
 
 namespace SalesWebMvc.Controllers
 {
@@ -12,10 +13,12 @@ namespace SalesWebMvc.Controllers
     {
 
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService) // Injeção de dependência 
+        public SellersController(SellerService sellerService, DepartmentService departmentService) // Injeção de dependência 
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
         public IActionResult Index() // chamou o controlador 
         {
@@ -24,7 +27,9 @@ namespace SalesWebMvc.Controllers
         }
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -21,8 +21,7 @@ namespace SalesWebMvc.Services
             return _context.Seller.ToList(); // Operação sincrona (aplicação fica bloqueada esperando operação terminar)
         }
         public void Insert(Seller obj)
-        {
-            obj.Department = _context.Department.First();
+        {      
             _context.Add(obj);
             _context.SaveChanges();
         }
